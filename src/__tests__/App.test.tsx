9f79bece4b8e4048ba8f3cb7267fdf518cc87729
paul <70586770+m4rtinpf@users.noreply.github.com>
2023-04-13 19:49:42 -0300

    add imports to test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -2,6 +2,7 @@ import {render, screen} from '@testing-library/react'
 import App from '../App'
 import { React } from "react";
 import '@testing-library/jest-dom'
+import { Stage, Layer, Rect } from "react-konva";
 
 test('loads and displays greeting', async () => {
   // ARRANGE
